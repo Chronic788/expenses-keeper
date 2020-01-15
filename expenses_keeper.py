d2d16268d2f9a7782dd03cb487a10d67d2090a0a
Jared Nelsen <jared.nelsen@plexus.com>
2020-01-15 11:12:27 -0700

    Worked on adding an expense and put the main menu in a loop.

--- expenses_keeper.py
+++ expenses_keeper.py
@@ -2,11 +2,18 @@
 import os.path
 from os import path
 
+# When I try to select the category when adding an expense it reads it off one line.
+# I need to figure out how to write the categories on separate lines
+
 def addAnExpense():
     
-    print("You have chosen to add an expense...\n")
+    print("\nYou have chosen to add an expense...\n")
 
     categories_file_name = "expense-categories.txt"
+    if not os.path.exists(categories_file_name):
+        print("Categories file does not exist! Create it by adding a category in the main menu.\n")
+        return
+
     categories_file = open(categories_file_name, "r+")
     categories = categories_file.readlines()
 
@@ -17,23 +24,23 @@ def addAnExpense():
 
     while True:
 
-        date = input("Enter the expense date:")
-        description = input("What was the expense for?")
-        amount = input("How much was the expense?")
+        date = input("Enter the expense date: ")
+        description = input("What was the expense for? ")
+        amount = input("How much was the expense? ")
         amount = amount.replace("$", "")
-        print("Select the category of the expense:")
+        print("Select the category of the expense: ")
         index = 1
         for c in categories:
-            print(index + ". " + c)
+            print(str(index) + ". " + c)
             index = index + 1
         category_index = input("")
-        category = categories[category_index - 1]
+        category = categories[int(category_index) - 1]
 
-        print("Here is your expense summary:")
-        print("Date: " + date)
-        print("Description: " + description)
-        print("Amount: " + amount)
-        print("Category: " + category)
+        print("\nHere is your expense summary:")
+        print(" Date: " + date)
+        print(" Description: " + description)
+        print(" Amount: " + amount)
+        print(" Category: " + category)
         
         record = input("\nRecord this expense?\n1 = Yes\n2 = No\n\n")
         if record == "1":
@@ -45,7 +52,26 @@ def addAnExpense():
             for i in range(len(categories) - int(category_index) - 1):
                 expense = expense + ","
 
-            # Write out expense here. include and manage header
+            expense_file_name = "expenses.csv"
+            expense_file = open(expense_file_name, "w+")
+            expense_lines = expense_file.readlines()
+
+            expense_write_lines = []
+
+            # Write out header
+            if len(expense_lines) == 0:
+                categories_line = "Date,Description,Amount,"
+                for i in range(len(categories)):
+                    categories_line = categories_line + categories[i]
+                    categories_line = categories_line + ","
+                categories_line = categories_line + "\n"
+                expense_write_lines.append(categories_line)
+
+            expense_write_lines.append(expense)
+
+            expense_file.writelines(expense_write_lines)
+
+            expense_file.close()
 
             print("Expense recorded...")
         else:
@@ -86,8 +112,10 @@ def viewCategories():
     option = input("Would you like to add a Category?\n1 = Yes\n2 = No\n")
     if option == "1":
         while True:
-            categories_file.write(input("\nWhat Category would you like to add?\n"))
-            print("Category added...\n")
+            new_category = []
+            new_category.append(input("\nWhat Category would you like to add?\n"))
+            categories_file.writelines(new_category)
+            print("\nCategory added...\n")
             another = input("Add another?\n1 = Yes\n2 = No\n")
             if another == "1":
                 continue
@@ -98,20 +126,27 @@ def viewCategories():
 
 def main():
 
-    print("Welcome to your Expenses Keeper!")
-    print("Choose an option:")
-    print("1. Add an expense")
-    print("2. View Expenses")
-    print("3. View and add Categories")
+    print("\nWelcome to your Expenses Keeper!")
 
-    option = input("")
+    while True:
+        print()
+        print("Choose an option:")
+        print("1. Add an expense")
+        print("2. View Expenses")
+        print("3. View and add Categories")
+        print("4. Exit")
 
-    if option == "1":
-        addAnExpense()
-    elif option == "2":
-        viewExpenses()
-    elif option == "3":
-        viewCategories()
+        option = input("")
+
+        if option == "1":
+            addAnExpense()
+        elif option == "2":
+            viewExpenses()
+        elif option == "3":
+            viewCategories()
+        elif option == "4":
+            print("\nExiting Expenses Keeper...\n")
+            break
 
 if __name__ == "__main__":
     main()
